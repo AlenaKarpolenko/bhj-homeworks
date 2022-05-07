9b781359af1cef068be0e96eb26da78d822324db
AlenaKarpolenko <alenac190590@mail.ru>
2022-05-07 13:07:30 +0700

    добавлено решение

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -15,16 +15,24 @@ class Game {
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
-
-  registerEvents() {
-    /*
+ /*
       TODO:
       Написать обработчик события, который откликается
       на каждый введённый символ.
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+  registerEvents() {
+  
+    document.addEventListener('keypress', event => {
+      if (this.currentSymbol.textContent === event.key) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    })
   }
+
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
